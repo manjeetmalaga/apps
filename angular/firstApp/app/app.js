a5605a85ec9d73660d688ef8e0e23eeba942c259
Manjeet Malaga <mann.jeet@gmail.com>
2019-06-01 05:44:04 -0400

    Updated other apps

---
+++ angular/firstApp/app/app.js
@@ -0,0 +1,15 @@
+'use strict'; 
+
+const myNinjaApp = angular.module('myNinjaApp', []);
+
+myNinjaApp.config(() => {
+
+});
+
+myNinjaApp.run(() => {
+
+});
+
+myNinjaApp.controller('NinjaController', () => {
+    
+});
